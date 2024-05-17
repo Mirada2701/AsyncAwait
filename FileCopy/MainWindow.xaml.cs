05d6e1d0869f590b99ed22edf09e3e94abfa519e
Віталій Лабенський <vitaliy.laben@gmail.com>
2024-05-18 00:07:05 +0300

    Add Material Design

--- FileCopy/MainWindow.xaml.cs
+++ FileCopy/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Win32;
+﻿using IOExtensions;
+using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Dialogs;
+using PropertyChanged;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 using System.Windows;
@@ -19,25 +22,65 @@ namespace FileCopy
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string Source { get; set; }
-        public string Destination { get; set; }
+        ViewModel model;
         public MainWindow()
         {
             InitializeComponent();
-            srcTextBox.Text = Source = "E:\\ШАГ\\3 семестр\\WF\\Text\\Friend.txt";
-            destTextBox.Text = Destination = "E:\\ШАГ\\3 семестр\\WF\\Text\\Directory\\Loshara";
+            model = new ViewModel()
+            {
+                Source = "C:\\Users\\M I R A D A\\Downloads\\1GB.bin",
+                Destination = "E:\\ШАГ\\3 семестр\\WF\\Text\\Directory",
+                Progress = 0
+            };
+            this.DataContext = model;
 
         }
 
-        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        private async void CopyButton_Click(object sender, RoutedEventArgs e)
         {
             //copy file from source to destination
-            string filename = System.IO.Path.GetFileName(srcTextBox.Text);
+            string filename = System.IO.Path.GetFileName(model.Source);
             
-            string destFilePath = System.IO.Path.Combine(Destination, filename);
-            File.Copy(Source, destFilePath, true);
+            string destFilePath = System.IO.Path.Combine(model.Destination, filename);
+            CopyProcessInfo info = new CopyProcessInfo(filename);
+            model.AddProcess(info);
+            await CopyFileAsync(model.Source, destFilePath,info);
+            info.Percentage = 100;
             MessageBox.Show("Complete!");
 
+        }
+        private Task CopyFileAsync(string src, string dest, CopyProcessInfo info)
+        {
+            //3 - FileTransferManager
+            return FileTransferManager.CopyWithProgressAsync(src, dest, (progress) =>
+            {
+                model.Progress = progress.Percentage;
+                info.Percentage = progress.Percentage;
+                info.BytesPerSecond = progress.BytesPerSecond;
+            }, false);
+            #region Copy File Hand
+            //return Task.Run(() =>
+            // {
+            //1 - using File Class
+            //File.Copy(Source, destFilePath, true);
+
+            //2 - FileStream
+            //using FileStream srcStream = new FileStream(src, FileMode.Open, FileAccess.Read);
+            //using FileStream destStream = new FileStream(dest, FileMode.Create, FileAccess.Write);
+            //int bytes = 0;
+            //byte[] buffer = new byte[1024 * 8];//8Kb
+            //do
+            //{
+            //    bytes = srcStream.Read(buffer, 0, buffer.Length);
+            //    destStream.Write(buffer, 0, bytes);
+            //    float percentage = destStream.Length / (srcStream.Length / 100);
+            //    model.Progress = percentage;
+            //} while (bytes > 0);
+            ////destStream.Close();
+            ////srcStream.Close();
+            ///
+            //});  
+            #endregion
         }
 
         private void Open_Source_Button_Click(object sender, RoutedEventArgs e)
@@ -45,8 +88,7 @@ namespace FileCopy
             OpenFileDialog dialog = new OpenFileDialog();
             if(dialog.ShowDialog() == true)
             {
-               Source =  dialog.FileName;
-               srcTextBox.Text = Source;
+               model.Source =  dialog.FileName;
             }
         }
 
@@ -56,10 +98,39 @@ namespace FileCopy
             dialog.IsFolderPicker = true;
             if(dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
-                Destination = dialog.FileName;
-                destTextBox.Text = Destination;
-            }
-            
+                model.Destination = dialog.FileName;
+            }            
+        }
+    }
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        private ObservableCollection<CopyProcessInfo> processes;
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public double Progress { get; set; }
+        public bool IsWaiting => Progress == 0;
+        public ViewModel()
+        {
+            processes = new ObservableCollection<CopyProcessInfo>();
+        }
+        public IEnumerable<CopyProcessInfo> Processes => processes;//get
+        public void AddProcess(CopyProcessInfo info)
+        {
+            processes.Add(info);
+        }
+    }
+    [AddINotifyPropertyChangedInterface]
+    public class CopyProcessInfo
+    {
+        public string FileName { get; set; }
+        public double Percentage { get; set; }
+        public int PercentageInt => (int)Percentage;
+        public double BytesPerSecond { get; set; }
+        public double MegaBytesPerSecond => Math.Round(BytesPerSecond / 1024 / 1024, 1);
+        public CopyProcessInfo(string filename)
+        {
+            this.FileName = filename;
         }
     }
 }
